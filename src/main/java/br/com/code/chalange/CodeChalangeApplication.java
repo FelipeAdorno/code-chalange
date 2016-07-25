74a4f2c9ef439d620f9f5117d22bfb1261d7924d
Felipe Adorno <felipeadsc@gmail.com>
2016-07-24 22:19:53 -0300

    Initialize project

---
+++ src/main/java/br/com/code/chalange/CodeChalangeApplication.java
@@ -0,0 +1,12 @@
+package br.com.code.chalange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChalangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeChalangeApplication.class, args);
+	}
+}
